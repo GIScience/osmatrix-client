6f54ee54384ec9d8a27c7e8329f16b857adceac9
rizzunis <rizwanunis@yahoo.com>
2016-09-13 14:29:50 +0200

    added c3 chart

--- js/ui.js
+++ js/ui.js
@@ -288,6 +288,158 @@ var Ui = (function (w) {
 	 * FEATURE INFO
 	 * *********************************************************************/
     
+    
+    function updateFeatureInfo(info, colors) {
+        $('#' + TOOLS.featureInfo + ' #chart').empty();
+        
+        if (info && colors) {
+            $('#' + TOOLS.featureInfo + ' h3').text(info.attribute.title);
+            $('#' + TOOLS.featureInfo + ' p').text(info.attribute.description);
+                   
+                   
+                   // var averages = [],
+              	// stddev = [],
+              	// min, max, xScale, yScale, xDomain = [];
+//                    
+//                    
+						    // // iterating through a dataset or arrays
+		            // for (var key in info.stats) {
+		                // min = d3.min([info.stats[key].min, min]);
+		                // max = d3.max([info.stats[key].max, max]);
+				// console.log("min: " + min + " max: "+ max);
+		            // }
+// 		            
+		            // if (info.attribute === 'DateOfLatestEdit' || info.attribute === 'dateOfEldestEdit') 
+		            		// {
+		            			// MARGIN_LEFT += 20; 
+		            		// }
+		                // else 
+		                	// {
+		                		// min = 0; 
+		                	// }
+// 		            
+		            // for (var i = 0, len = info.timestamps.length; i < len; i++) {
+		                // xDomain.push(info.timestamps[i].timestamp.substring(0, info.timestamps[i].timestamp.lastIndexOf('-')));
+		            // }
+						    // for (var key in info.stats) 
+						    // {
+		                // averages.push({"timestamp": key, "value": info.stats[key].avg});
+		                // stddev.push({"timestamp": key, "upper": info.stats[key].avg + info.stats[key].std, "lower": info.stats[key].avg - info.stats[key].std});
+		           		// }
+//             
+				            // ;
+                   
+                   
+                   function convert(info){
+
+						  var out=[];
+						  
+						  
+						  for (var i = 0; i < info.timestamps.length;i++){
+						  
+						    var ts = {};
+						    
+						   
+						    
+						    
+						    
+						    ts["ts"] = info.timestamps[i].timestamp;
+						    
+						    for (var j = 0; j < info.result.length; j++){
+						      ts["cell_" + j] = info.result[j].values[ts["ts"]];
+						    }
+						    
+						    
+						    ts["cell_avg"] = parseFloat(info.stats[ts["ts"]].avg.toFixed(2));
+						    
+						    ts["cell_std"] = parseFloat(info.stats[ts["ts"]].std.toFixed(2));
+						    
+						    
+						    out.push(ts);
+						  }
+						  
+						  
+						  return out;
+						
+						}
+                   
+                   
+                   
+                    var chart = c3.generate({
+                    					  bindTo:"#chart",
+                    					  zoom: {
+										        enabled: true
+										    },
+										  data: {
+										  	
+										    json: convert(info),
+										    keys: {
+										       x: 'ts', // it's possible to specify 'x' when category axis
+										       value: ['cell_avg','cell_0', 'cell_1','cell_2', 'cell_3','cell_4', 'cell_5','cell_6'],
+										    },
+										    names:{
+										    	'cell_avg': "average",
+										    	'cell_0': " ",
+										    	'cell_1': " ",
+										    	'cell_2': " ",
+										    	'cell_3': " ",
+										    	'cell_4': " ",
+										    	'cell_5': " ",
+										    	'cell_6': " " 
+										    },
+										    colors: {
+										    	'cell_avg': '#000000',
+										    	'cell_0': colors[0],
+										    	'cell_1': colors[1],
+										    	'cell_2': colors[2],
+										    	'cell_3': colors[3],
+										    	'cell_4': colors[4],
+										    	'cell_5': colors[5],
+										    	'cell_6': colors[6] 
+										    	
+										    },
+										  },
+										  
+										  axis: {
+										    x: {
+										      type: 'timeseries',
+										      tick: {
+									                format: '%Y-%m-%d',
+									                rotate: 90
+									            }
+										    }
+										  },
+										  tooltip: {
+											  position: function (data, width, height, element) {
+											    return {top: 0, left: 355};
+											  }
+											}
+										});  
+               
+      
+           
+            // **********************
+            // OPEN THE THING, YO!
+            
+            if (!$('#' + TOOLS.featureInfo + '> .content').hasClass('active')) {$('#' + TOOLS.featureInfo + ' button').click(); }
+       }  else {
+            $('#' + TOOLS.featureInfo + ' h3').text('');
+            $('#' + TOOLS.featureInfo + ' p').text('Click on the map to get information on the temporal evolution of the selected characteristic in the area of interest.');
+       		
+        }
+        setLoadingState(false, TOOLS.featureInfo);
+    };
+    
+    function setFeatureInfoLoadingState(state) {
+        setLoadingState(state, TOOLS.featureInfo);
+    }
+    
+    //:::::::::::::::::::::::
+    
+    
+   /* 
+    
+    
     function updateFeatureInfo(info, colors) {
         $('#' + TOOLS.featureInfo + ' #chart').empty();
         
@@ -315,7 +467,21 @@ var Ui = (function (w) {
             	
                 averages = [],
                 stddev = [];
-                
+        
+        
+        
+        //:::::::::::::::.........................................
+ /*     var zoom = d3.behavior.zoom()
+    .x(xScale)
+    .y(yScale)
+    .scaleExtent([1, 10])
+    .on("zoom", zoomed);	
+        
+        //zoommmm        
+             //  var zoom = d3.behavior.zoom()
+			//    .x(x)
+			  //  .y(y)
+			   // .on("zoom", zoomed);
   	          	
             	// iterating through a dataset or arrays
             for (var key in info.stats) {
@@ -338,8 +504,25 @@ var Ui = (function (w) {
             }
             
             
+           /*  var zoom = d3.behavior.zoom()
+				    .x(xScale)
+				    .on("zoom", function() {
+				        if (xScale.domain()[0] < xDomain) {
+					    var x = zoom.translate()[0] - xScale(xDomain) + xScale.range()[0];
+				            zoom.translate([x, 0]);
+				        } else if (xScale.domain()[1] > xDomain) {
+					    var x = zoom.translate()[0] - xScale(xDomain) + xScale.range()[1];
+				            zoom.translate([x, 0]);
+				        }
+				        redrawChart();
+       // updateViewportFromChart();
+    });
+          
+            
             // scales, domain and ranges to adjust our visualization to match our data..........
             // D3 ...scales are represented using domain and ranges
+           
+           
             // variable xScale & yScale
             xScale = d3.scale.ordinal()
 	            .domain(xDomain)
@@ -354,7 +537,34 @@ var Ui = (function (w) {
             	.append("svg")
                 .attr("width", WIDTH)
                 .attr("height", HEIGHT);
-                  
+               
+     			// .append("g")
+      
+      
+     // svg.append("line")
+     
+             
+             //.append("g")
+  			  //.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    		//.call(zoom);
+
+ /* var zoom = d3.behavior.zoom()
+    .on("zoom", draw);
+    
+    
+    chartArea.append("clipPath")
+    .attr("id", "clip")
+  .append("rect")
+    .attr("x", x(0))
+    .attr("y", y(1))
+    .attr("width", x(1) - x(0))
+    .attr("height", y(0) - y(1));*/
+			  /*  .call(zoom)
+			    .append("g")
+			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                
+                
+                //.....................................................................................  
            tooltip = d3.select("div#chart")
 				.append("div")
 				.style("position", "absolute")
@@ -362,7 +572,14 @@ var Ui = (function (w) {
 				.style("font-size", "16px")
 				.style("z-index", "10")
 				.style("visibility", "hidden");
-			           
+			           //................................................................................
+//::::::::::::::....................................................................................          
+         
+          
+        
+      
+      
+     
             // **********************
             // PLOT STATISTICAL MEASURES
             
@@ -384,6 +601,7 @@ var Ui = (function (w) {
                     .attr("stroke", '#bbb')
                     .attr("stroke-width", 2)
                     .attr("fill", "transparent");
+                    
             
             
             // **********************
@@ -408,7 +626,10 @@ var Ui = (function (w) {
                     .attr("y1", (HEIGHT - MARGIN_BOTTOM))
                     .attr("y2", (HEIGHT - MARGIN_BOTTOM))
                     .style("stroke", "#000")
-                    .style("stroke-width", "1");
+                    .style("stroke-width", "1")
+                    ;
+                  //  .append('g')
+                   //  .attr("transform", "translate(" + width + ",0)");
            
            // add label by adding text elements to SVG element
             chartArea.selectAll(".yLabel")
@@ -438,7 +659,10 @@ var Ui = (function (w) {
                     .attr("x2", function(d) {return xScale(d);})
                     .attr("y1", HEIGHT - MARGIN_BOTTOM)
                     .attr("y2", HEIGHT - MARGIN_BOTTOM + 5)
-                    .style("stroke", "#000");
+                    .style("stroke", "#000")
+                    ;
+                   //  .append('g')
+                     //.attr("transform", "translate(0," + height + ")");
     
     			
     				
@@ -465,12 +689,35 @@ var Ui = (function (w) {
                 for (var key in item.values) {
                     value.push({"timestamp": key, "value": item.values[key]});
                 }
-                console.log(value);
-                chartArea.append("svg:path")
+               // console.log(value);
+              
+              
+               //data bound to line...................................................................................................................
+           // d3.select("#start").on("click", function() {
+              var path =  chartArea.append("svg:path") // for unrolling line time animation
                     .attr("d", line(value))
                     .attr("stroke", colors[i])
-                    .attr("fill", "transparent");
-                
+                    .attr("fill", "transparent")
+                    ;
+                    
+   
+                    
+              // Variable to Hold Total Length
+			    var totalLength = path.node().getTotalLength();
+			
+			    // Set Properties of Dash Array and Dash Offset and initiate Transition
+			    path
+			      .attr("stroke-dasharray", totalLength + " " + totalLength)
+			      .attr("stroke-dashoffset", totalLength)
+			     .transition() // Call Transition Method
+			      .duration(4000) // Set Duration timing (ms)
+			      .ease("linear") // Set Easing option
+			      .attr("stroke-dashoffset", 0); // Set final value of dash-offset for transition...........................................................
+			      
+                      
+		
+					 
+             
                 chartArea.selectAll("path" + item.cell_id).data(value)
                     .enter().append("svg:circle")
                     .attr("class", "path" + item.cell_id)
@@ -480,28 +727,57 @@ var Ui = (function (w) {
                     .attr("fill", "#ffffff")
                     .attr("stroke", colors[i])
                     .attr("stroke-width", 2)
-                    
-                    // testing event listeners
+                   
+                   
+                    // testing event listeners...................................................................................
                     
                     .on("mouseover", function(d){
-						return tooltip.style("visibility", "visible").text(d.timestamp + " :  " + d.value);
+						return tooltip.style("visibility", "visible").text(d.timestamp + ": " + d.value);
 					})
 					.on("mousemove", function(d){
-						return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px").text(d.timpestamp + " :  " + d.value);
+						return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px").text(d.timpestamp + ": " + d.value);
 					})
 					.on("mouseout", function(d){
 						return tooltip.style("visibility", "hidden");
 					});
-                  
+					
+					
+					
+					// var zoom = d3.behavior.zoom()
+  			  //.on("zoom", draw);
+						
+					/*	  var xyzoom = d3.behavior.zoom()
+						      .x(xscale)
+						      .y(yscale)
+						      .on("zoom", zoomable ? draw : null);
+						    var xzoom = d3.behavior.zoom()
+						      .x(xscale)
+						      .on("zoom", zoomable ? draw : null);
+						    var yzoom = d3.behavior.zoom()
+						      .y(yscale)
+						      .on("zoom", zoomable ? draw : null);
+
+				function draw() {
+				 // svg.select("g.x.axis").call(xAxis);
+				 // svg.select("g.y.axis").call(yAxis);
+				 // svg.select("path.area").attr("d", stddevArea);
+				  svg.select("path.line").attr("d", line);
+				} ;              
+					    	*/
+						
+						           // });
+                  /* // Reset Animation
+					d3.select("#reset").on("click", function() {
+						d3.select(".line").remove();
+					});...........................................................................................................................
                }
-                      
+               
+                  */  
 				   
-				           
-           
             // **********************
             // OPEN THE THING, YO!
             
-            if (!$('#' + TOOLS.featureInfo + '> .content').hasClass('active')) {$('#' + TOOLS.featureInfo + ' button').click(); }
+ /*           if (!$('#' + TOOLS.featureInfo + '> .content').hasClass('active')) {$('#' + TOOLS.featureInfo + ' button').click(); }
         } else {
             $('#' + TOOLS.featureInfo + ' h3').text('');
             $('#' + TOOLS.featureInfo + ' p').text('Click on the map to get information on the temporal evolution of the selected characteristic in the area of interest.');
@@ -513,7 +789,7 @@ var Ui = (function (w) {
     function setFeatureInfoLoadingState(state) {
         setLoadingState(state, TOOLS.featureInfo);
     }
-    
+    */
     /* *********************************************************************
 	 * CONSTRUCTOR
 	 * *********************************************************************/
